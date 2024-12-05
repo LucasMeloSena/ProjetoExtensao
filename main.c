9b9b9d16e49d9789fea11656f16e2171afaef98b
Lucas Sena <lucasmelosena@gmail.com>
2024-12-05 16:50:45 -0300

    fix: resolve code errors

--- main.c
+++ main.c
@@ -4,19 +4,19 @@
 #include <locale.h>
 #include <string.h>
 
-#define NUM_MAX_CAD 1024
+#define NUM_MAX_CADASTROS 1024
 
-typedef struct Aluno {
+typedef struct
+{
     int ocupado;
-	char nome[256];
-	int idade;
-	char sexo;
-	char 
+    char nome[256];
+    int idade;
+    char sexo;
+    char matricula[12]
 } Aluno;
 
-
-Aluno vetor_pessoas[NUM_MAX_CAD];
-int total_pessoas = 0;
+Aluno vetor_alunos[NUM_MAX_CADASTROS];
+int total_alunos = 0;
 
 int tela_menu_opcoes();
 void tela_cadastrar();
@@ -24,7 +24,8 @@ void tela_pesquisar();
 void tela_remover();
 void tela_editar();
 int tela_sair();
-
+void limpar();
+void pausar();
 
 int main()
 {
@@ -34,41 +35,43 @@ int main()
     int sair = 0;
     int opcao;
 
-    for(int i=0; i < NUM_MAX_CAD; i++)
+    for (int i = 0; i < NUM_MAX_CADASTROS; i++)
     {
-        vetor_pessoas[i].ocupado = 0;
+        vetor_alunos[i].ocupado = 0;
     }
 
-    while (1) {
+    while (1)
+    {
         opcao = tela_menu_opcoes();
 
-        switch(opcao)
+        switch (opcao)
         {
-            case 1:
-                tela_cadastrar();
+        case 1:
+            tela_cadastrar();
             break;
-            case 2:
-                tela_pesquisar();
+        case 2:
+            tela_pesquisar();
             break;
-            case 3:
-                tela_remover();
+        case 3:
+            tela_remover();
             break;
-            case 4:
-                tela_editar();
+        case 4:
+            tela_editar();
             break;
-            case 5:
-                sair = tela_sair();
-                if (sair == 1) {
-                    exit(0);
-                }
+        case 5:
+            sair = tela_sair();
+            if (sair == 1)
+            {
+                exit(0);
+            }
             break;
-            default:
-                printf("\nOpção Inválida!!! \n");
-                system("pause");
+        default:
+            printf("\nOpção Inválida!!! \n");
+            pausar();
         }
     }
 
-    system("cls");
+    limpar();
     printf("\nFim da execu��o do programa!\n");
 
     return 0;
@@ -78,7 +81,7 @@ int tela_menu_opcoes()
 {
     int opcao;
 
-    system("cls");
+    limpar();
     printf("Menu de Opções \n");
     printf("1 - Cadastrar \n");
     printf("2 - Pesquisar \n");
@@ -89,7 +92,8 @@ int tela_menu_opcoes()
     printf("Escolha uma opçãoo: ");
     int erro = scanf("%d", &opcao);
 
-    if(erro != 1) {
+    if (erro != 1)
+    {
         fflush(stdin);
         opcao = -1;
     }
@@ -105,19 +109,19 @@ void tela_cadastrar()
     int idxPesquisa = -1;
 
     char nome[256];
-    char cpf[13];
-	int idade;
-	char sexo, strSexo[3];
-    float salario;
+    int idade;
+    char sexo, strSexo[3];
+    char matricula[12];
 
-    do {
-        system("cls");
+    do
+    {
+        limpar();
         printf("1 - Cadastrar \n");
 
-        if(total_pessoas >= NUM_MAX_CAD)
+        if (total_alunos >= NUM_MAX_CADASTROS)
         {
-            printf("\nErro: O cadastro j� atingiu sua capacidade m�xima!\n");
-            system("pause");
+            printf("\nErro: O cadastro já atingiu sua capacidade m�xima!\n");
+            pausar();
             break;
         }
 
@@ -127,40 +131,20 @@ void tela_cadastrar()
 
             printf("\nDigite o nome: ");
             scanf(" %255[^\n]", nome);
-
             fflush(stdin);
 
-
-            if(strlen(nome) > 254)
+            if (strlen(nome) > 254)
             {
                 erro = 1;
-                printf("ERRO: O nome pode possuir no m�ximo 254 carracteres!\n");
+                printf("ERRO: O nome pode possuir no máximo 254 carracteres!\n");
             }
-            if(strlen(nome) < 3) {
-                erro = 1;
-                printf("ERRO: O nome deve possuir pelo menos 3 (tr�s) carracteres!\n");
-            }
-
-        } while(erro == 1);
-
-        do
-        {
-            erro = 0;
-
-            printf("\nDigite o CPF: ");
-
-            scanf(" %12[^\n]", cpf);
-
-            fflush(stdin);
-
-            if(strlen(cpf) != 11)
+            if (strlen(nome) < 3)
             {
                 erro = 1;
-                printf("ERRO: O CPF deve possuir 11 (onze) d�gitos!\n");
+                printf("ERRO: O nome deve possuir pelo menos 3 (três) carracteres!\n");
             }
 
-        }while(erro == 1);
-
+        } while (erro == 1);
 
         do
         {
@@ -169,30 +153,27 @@ void tela_cadastrar()
 
             fflush(stdin);
 
-            if(erro != 1)
+            if (erro != 1)
             {
-                printf("ERRO: Digite um valor inteiro v�lido!\n");
+                printf("ERRO: Digite um valor inteiro válido!\n");
             }
-            else if(idade < 1 || idade > 120)
+            else if (idade < 1 || idade > 20)
             {
                 erro = -1;
-                printf("ERRO: A idade deve estar entre 1 e 120 anos!\n");
+                printf("ERRO: A idade deve estar entre 1 e 20 anos!\n");
             }
 
-        }while(erro != 1);
-
+        } while (erro != 1);
 
         do
         {
             erro = 0;
 
             printf("\nDigite o sexo (M / F): ");
-
             scanf(" %2[^\n]", strSexo);
-
             fflush(stdin);
 
-            if(strlen(strSexo) == 1)
+            if (strlen(strSexo) == 1)
             {
                 sexo = toupper(strSexo[0]);
             }
@@ -200,38 +181,21 @@ void tela_cadastrar()
             {
                 erro = 1;
             }
-
-            if( !(sexo == 'M' || sexo == 'F') || erro == 1 )
+            if (!(sexo == 'M' || sexo == 'F') || erro == 1)
             {
                 erro = 1;
-                printf("ERRO: S�o aceitos apenas os valores 'M' ou 'F'!\n");
+                printf("ERRO: São aceitos apenas os valores 'M' ou 'F'!\n");
             }
 
-        }while(erro == 1);
-
-
-        do
-        {
-            printf("\nDigite o sal�rio: R$ ");
-            erro = scanf("%f", &salario);
-
-            fflush(stdin);
-
-            if(salario < 1320.0 || erro != 1)
-            {
-                erro = -1;
-                printf("ERRO: o sal�rio n�o pode ser menor que R$ 1320,00 !\n");
-            }
-
-        }while(erro != 1);
+        } while (erro == 1);
 
         idxPesquisa = -1;
 
-        for(int i=0; i < NUM_MAX_CAD; i++)
+        for (int i = 0; i < NUM_MAX_CADASTROS; i++)
         {
-            if(vetor_pessoas[i].ocupado == 1) {
-
-                if( strcmp(vetor_pessoas[i].cpf, cpf) == 0)
+            if (vetor_alunos[i].ocupado == 1)
+            {
+                if (strcmp(vetor_alunos[i].matricula, matricula) == 0)
                 {
                     idxPesquisa = i;
                     break;
@@ -239,43 +203,38 @@ void tela_cadastrar()
             }
         }
 
-
-        if(idxPesquisa != -1)
+        if (idxPesquisa != -1)
         {
-            printf("\nErro: N�o foi poss�vel inserir os dados pois j� existe uma pessoa com o mesmo cpf no cadastro:\v");
+            printf("\nErro: Não foi possível inserir os dados pois já existe um aluno com a mesma matrícula no cadastro:\v");
 
-            printf("\nPessoa com o mesmo CPF: \n");
-            printf("CPF:     %s\n", vetor_pessoas[idxPesquisa].cpf);
-            printf("Nome:    %s\n", vetor_pessoas[idxPesquisa].nome);
-            printf("Idade:   %d\n", vetor_pessoas[idxPesquisa].idade);
-            printf("Sexo:    %c\n", vetor_pessoas[idxPesquisa].sexo);
-            printf("Salario: R$ %.2f\n\n",vetor_pessoas[idxPesquisa].salario);
-            system("pause");
+            printf("\nAluno com a mesma matrícula: \n");
+            printf("Nome:    %s\n", vetor_alunos[idxPesquisa].nome);
+            pausar();
         }
         else
         {
-            for(int i=0; i < NUM_MAX_CAD; i++)
+            for (int i = 0; i < NUM_MAX_CADASTROS; i++)
             {
-                if(vetor_pessoas[i].ocupado == 0)
+                if (vetor_alunos[i].ocupado == 0)
                 {
-                    vetor_pessoas[i].ocupado = 1;
+                    vetor_alunos[i].ocupado = 1;
 
-                    strcpy(vetor_pessoas[i].nome, nome);
-                    strcpy(vetor_pessoas[i].cpf, cpf);
-                    vetor_pessoas[i].idade = idade;
-                    vetor_pessoas[i].sexo = sexo;
-                    vetor_pessoas[i].salario = salario;
-                    total_pessoas = total_pessoas + 1;
+                    strcpy(vetor_alunos[i].nome, nome);
+                    strcpy(vetor_alunos[i].matricula, matricula);
+                    vetor_alunos[i].idade = idade;
+                    vetor_alunos[i].sexo = sexo;
+                    total_alunos = total_alunos + 1;
 
                     printf("\nDados cadastrados com sucesso! \n");
-                    system("pause");
+                    pausar();
 
                     break;
                 }
             }
         }
 
-        do{
+        do
+        {
 
             printf("\nGostaria de cadastrar outra pessoa? (s/n): ");
             scanf(" %c", &ch);
@@ -284,153 +243,118 @@ void tela_cadastrar()
 
             ch = toupper(ch);
 
-        }while(!(ch == 'S' || ch == 'N'));
+        } while (!(ch == 'S' || ch == 'N'));
 
-        if(ch == 'S')
+        if (ch == 'S')
         {
             continuar = 1;
         }
-        else{
+        else
+        {
             continuar = 0;
         }
 
-    } while(continuar == 1);
+    } while (continuar == 1);
 }
 
 void tela_pesquisar()
 {
     int continuar;
-
     char ch;
-
     int erro;
-
     int idxPesquisa = -1;
+    char matricula[12];
 
-    char cpf[13];
+    do
+    {
 
-    do{
-
-        system("cls");
+        pausar();
         printf("2 - Pesquisar \n");
 
-        do
-        {
-            erro = 0;
+        erro = 0;
 
-            printf("\nDigite o CPF: ");
-            scanf(" %12[^\n]", cpf);
-
-            fflush(stdin);
-
-            if(strlen(cpf) != 11)
-            {
-                erro = 1;
-                printf("ERRO: O CPF deve possuir 11 (onze) d�gitos!\n");
-            }
-
-        }while(erro == 1);
+        printf("\nDigite a matrícula: ");
+        scanf(" %12[^\n]", matricula);
+        fflush(stdin);
 
         int idxPesquisa = -1;
-
-        for(int i=0; i<NUM_MAX_CAD; i++){
-
-            if(vetor_pessoas[i].ocupado == 1)
+        for (int i = 0; i < NUM_MAX_CADASTROS; i++)
+        {
+            if (vetor_alunos[i].ocupado == 1)
             {
-                if(strcmp(vetor_pessoas[i].cpf, cpf) == 0){
-
+                if (strcmp(vetor_alunos[i].matricula, matricula) == 0)
+                {
                     idxPesquisa = i;
-
                     break;
                 }
             }
         }
 
-        if(idxPesquisa != -1)
+        if (idxPesquisa != -1)
         {
-            printf("\nPessoa Encontrada: \n");
-            printf("CPF:     %s\n", vetor_pessoas[idxPesquisa].cpf);
-            printf("Nome:    %s\n", vetor_pessoas[idxPesquisa].nome);
-            printf("Idade:   %d\n", vetor_pessoas[idxPesquisa].idade);
-            printf("Sexo:    %c\n", vetor_pessoas[idxPesquisa].sexo);
-            printf("Salario: R$ %.2f\n\n",vetor_pessoas[idxPesquisa].salario);
-            system("pause");
+            printf("\Aluno encontrado: \n");
+            printf("Matrícula:     %s\n", vetor_alunos[idxPesquisa].matricula);
+            printf("Nome:    %s\n", vetor_alunos[idxPesquisa].nome);
+            printf("Idade:   %d\n", vetor_alunos[idxPesquisa].idade);
+            printf("Sexo:    %c\n", vetor_alunos[idxPesquisa].sexo);
+            pausar();
         }
         else
         {
-            printf("\nN�o foi encontrada uma pessoa com o cpf informado!\n\n");
-            system("pause");
+            printf("\nNão foi encontrado um aluno com a matrícula informada!\n\n");
+            pausar();
         }
 
-        do{
+        do
+        {
 
             printf("\nGostaria de pesquisar outra pessoa?  (s/n): ");
             scanf(" %c", &ch);
-
             fflush(stdin);
 
             ch = toupper(ch);
 
-        }while(!(ch == 'S' || ch == 'N'));
+        } while (!(ch == 'S' || ch == 'N'));
 
-        if(ch == 'S')
+        if (ch == 'S')
         {
             continuar = 1;
         }
-        else{
+        else
+        {
             continuar = 0;
         }
 
-    } while(continuar == 1);
+    } while (continuar == 1);
 }
 
 void tela_remover()
 {
     int continuar;
-
     char ch;
-
     int erro;
-
     int idxPesquisa = -1;
-
-    char cpf[13];
+    char matricula[12];
 
     do
     {
-        system("cls");
+        limpar();
         printf("3 - Remover \n\n");
 
-        do
-        {
-            erro = 0;
-
-            printf("\nDigite o CPF: ");
-
-            scanf(" %12[^\n]", cpf);
-
-            fflush(stdin);
-
-            if(strlen(cpf) != 11)
-            {
-                erro = 1;
-                printf("ERRO: O CPF deve possuir 11 (onze) d�gitos!\n");
-            }
-
-        }while(erro == 1);
-
+        erro = 0;
+        printf("\nDigite a matrícula: ");
+        scanf(" %11[^\n]", matricula);
+        fflush(stdin);
 
         idxPesquisa = -1;
-
-        for(int i = 0; i < NUM_MAX_CAD; i++) {
-
-            if(vetor_pessoas[i].ocupado == 1)
+        for (int i = 0; i < NUM_MAX_CADASTROS; i++)
+        {
+            if (vetor_alunos[i].ocupado == 1)
             {
-                if(strcmp(vetor_pessoas[i].cpf, cpf) == 0){
-
-                    total_pessoas = total_pessoas - 1;
-
-                    vetor_pessoas[i].ocupado = 0;
+                if (strcmp(vetor_alunos[i].matricula, matricula) == 0)
+                {
+                    total_alunos = total_alunos - 1;
+                    vetor_alunos[i].ocupado = 0;
                     idxPesquisa = i;
 
                     break;
@@ -438,18 +362,18 @@ void tela_remover()
             }
         }
 
-        if(idxPesquisa != -1)
+        if (idxPesquisa != -1)
         {
-            printf("\nPessoa removida com sucesso!\n");
+            printf("\Aluno removido com sucesso!\n");
         }
         else
         {
-            printf("\nN�o foi poss�vel remover a pessoa do cadastro!\n");
+            printf("\nNão foi possível remover a pessoa do cadastro!\n");
         }
+        pausar();
 
-        system("pause");
-
-        do{
+        do
+        {
 
             printf("\nGostaria de remover outra pessoa?  (s/n): ");
             scanf(" %c", &ch);
@@ -458,39 +382,43 @@ void tela_remover()
 
             ch = toupper(ch);
 
-        }while(!(ch == 'S' || ch == 'N'));
+        } while (!(ch == 'S' || ch == 'N'));
 
-        if(ch == 'S')
+        if (ch == 'S')
         {
             continuar = 1;
         }
-        else{
+        else
+        {
             continuar = 0;
         }
 
-    }while(continuar);
+    } while (continuar);
 }
 
 void tela_editar()
 {
-    system("cls");
-    printf("4 - Relat�rio \n\n");
+}
 
-    printf("Total de Pessoas Cadastradas: %d\n\n", total_pessoas);
+void tela_relatorio()
+{
+    limpar();
+    printf("4 - Relatório \n\n");
 
-    for(int i=0; i<NUM_MAX_CAD; i++){
+    printf("Total de Alunos Cadastrados: %d\n\n", total_alunos);
 
-        if(vetor_pessoas[i].ocupado == 1)
+    for (int i = 0; i < NUM_MAX_CADASTROS; i++)
+    {
+        if (vetor_alunos[i].ocupado == 1)
         {
-            printf("CPF:     %s\n", vetor_pessoas[i].cpf);
-            printf("Nome:    %s\n", vetor_pessoas[i].nome);
-            printf("Idade:   %d\n", vetor_pessoas[i].idade);
-            printf("Sexo:    %c\n", vetor_pessoas[i].sexo);
-            printf("Salario: R$ %.2f\n\n",vetor_pessoas[i].salario);
+            printf("Matrícula:     %s\n", vetor_alunos[i].matricula);
+            printf("Nome:    %s\n", vetor_alunos[i].nome);
+            printf("Idade:   %d\n", vetor_alunos[i].idade);
+            printf("Sexo:    %c\n", vetor_alunos[i].sexo);
         }
     }
 
-    system("pause");
+    pausar();
 }
 
 int tela_sair()
@@ -498,10 +426,11 @@ int tela_sair()
     char ch;
 
     int sair;
-    system("cls");
+    limpar();
     printf("5 - Sair \n\n");
 
-    do{
+    do
+    {
         printf("\nDeseja sair?  (s/n): ");
         scanf(" %c", &ch);
 
@@ -509,16 +438,29 @@ int tela_sair()
 
         ch = toupper(ch);
 
-        if(ch == 'S')
+        if (ch == 'S')
         {
             sair = 1;
         }
-        else if(ch == 'N')
+        else if (ch == 'N')
         {
             sair = 0;
         }
 
-    }while(!(ch == 'S' || ch == 'N'));
+    } while (!(ch == 'S' || ch == 'N'));
 
     return sair;
+}
+
+void limpar()
+{
+    system("clear");
+    // system("cls");
+}
+
+void pausar()
+{
+    printf("Pressione Enter para continuar...");
+    getchar();
+    // system("pause");
 }
