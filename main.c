056cf25b6f3cee200d09842ed879d8cd57059fd7
Lucas Sena <lucasmelosena@gmail.com>
2024-12-06 21:12:04 -0300

    feat: separate differet user types

--- main.c
+++ main.c
@@ -3,8 +3,20 @@
 #include <ctype.h>
 #include <locale.h>
 #include <string.h>
+#include <time.h>
 
-#define NUM_MAX_CADASTROS 1024
+#define NUM_MAX_CADASTROS_ALUNOS 1024
+#define NUM_MAX_CADASTROS_ADMIN 100
+#define NUM_MAX_CADASTROS_CUIDADORES 1024
+#define NUM_MAX_CADASTROS_OBSERVACOES 3000
+
+typedef struct
+{
+    int ocupado;
+    char id[12];
+    char nome[256];
+    char senha[10];
+} Admin;
 
 typedef struct
 {
@@ -17,55 +29,128 @@ typedef struct
     char informacoes[1000];
 } Aluno;
 
-Aluno vetor_alunos[NUM_MAX_CADASTROS];
-int total_alunos = 0;
-char caminho_arquivo_alunos[256];
+typedef struct
+{
+    int ocupado;
+    char matricula[12];
+    char nome[256];
+    char senha[10];
+} Cuidador;
 
-int tela_menu_opcoes();
+typedef struct
+{
+    int ocupado;
+    char matricula_aluno[12];
+    char matricula_cuidador[12];
+    char observacao[2000];
+    char data[100];
+} Observacao;
+
+Admin vetor_admin[NUM_MAX_CADASTROS_ADMIN];
+Aluno vetor_alunos[NUM_MAX_CADASTROS_ALUNOS];
+Cuidador vetor_cuidadores[NUM_MAX_CADASTROS_CUIDADORES];
+Observacao vetor_observacoes[NUM_MAX_CADASTROS_OBSERVACOES];
+
+int total_admin = 0;
+int total_alunos = 0;
+int total_cuidadores = 0;
+int total_observacoes = 0;
+char caminho_arquivo_alunos[256];
+char caminho_arquivo_admin[256];
+char caminho_arquivo_cuidadores[256];
+char caminho_arquivo_observacoes[256];
+time_t agora;
+// agora = time(NULL);
+
+// Telas iniciais:
+int tela_menu_opcoes_home();
+int tela_menu_opcoes_cadastro_login();
+int tela_menu_opcoes_login();
+void tela_cadastrar_admin();
+int tela_login_admin();
+void tela_login_cuidador();
+int tela_menu_opcoes_admin();
+int tela_menu_opcoes_cuidador();
+
+// Telas admin:
 void tela_cadastrar_aluno();
 void tela_pesquisar_aluno_by_matricula();
 void tela_remover();
 void tela_editar();
-int tela_sair();
 void init_arquivo_alunos();
 void salvar_alunos_arquivo();
 void carregar_alunos_arquivo();
+void init_arquivo_admin();
+void salvar_admin_arquivo();
+void carregar_admin_arquivo();
+
+// Telas comuns
 void limpar();
 void pausar();
+int tela_sair();
 
 int main()
 {
     setlocale(LC_ALL, "portuguese");
     system("chcp 1252 > null");
+    init_arquivo_admin();
     init_arquivo_alunos();
 
     int sair = 0;
     int opcao;
 
-    for (int i = 0; i < NUM_MAX_CADASTROS; i++)
+    for (int i = 0; i < NUM_MAX_CADASTROS_ALUNOS; i++)
     {
+        vetor_admin[i].ocupado = 0;
         vetor_alunos[i].ocupado = 0;
+        vetor_cuidadores[i].ocupado = 0;
+        vetor_observacoes[i].ocupado = 0;
     }
 
     while (1)
     {
-        opcao = tela_menu_opcoes();
+        opcao = tela_menu_opcoes_home();
+        int opcao_cadastro_login;
+        int opcao_admin;
 
         switch (opcao)
         {
         case 1:
-            tela_cadastrar_aluno();
+            opcao_cadastro_login = tela_menu_opcoes_cadastro_login();
+            switch (opcao_cadastro_login)
+            {
+            case 1:
+                tela_cadastrar_admin();
+                break;
+            case 2:
+                opcao_admin = tela_login_admin();
+                switch (opcao_admin)
+                {
+                case 1:
+                    tela_cadastrar_aluno();
+                    break;
+                case 2:
+                    tela_pesquisar_aluno_by_matricula();
+                    break;
+                case 3:
+                    tela_remover();
+                    break;
+                case 4:
+                    tela_editar();
+                    break;
+                case 6:
+                    break;
+                default:
+                    printf("\nOpção Inválida!!! \n");
+                    pausar();
+                }
+                break;
+            }
             break;
         case 2:
-            tela_pesquisar_aluno_by_matricula();
+            // tela_login_admin();
             break;
         case 3:
-            tela_remover();
-            break;
-        case 4:
-            tela_editar();
-            break;
-        case 6:
             sair = tela_sair();
             if (sair == 1)
             {
@@ -85,7 +170,51 @@ int main()
     return 0;
 }
 
-int tela_menu_opcoes()
+int tela_menu_opcoes_home()
+{
+    int opcao;
+
+    limpar();
+    printf("Menu de Opções - Home \n");
+    printf("1 - Usuário Admin\n");
+    printf("2 - Usuário Cuidador\n");
+    printf("3 - Sair \n\n");
+
+    printf("Escolha um tipo de usuário para prosseguir: ");
+    int erro = scanf("%d", &opcao);
+
+    if (erro != 1)
+    {
+        fflush(stdin);
+        opcao = -1;
+    }
+
+    return opcao;
+}
+
+int tela_menu_opcoes_cadastro_login()
+{
+    int opcao;
+
+    limpar();
+    printf("Menu de Opções - Admin \n");
+    printf("1 - Cadastro\n");
+    printf("2 - Login\n");
+    printf("3 - Voltar \n\n");
+
+    printf("Escolha uma opção: ");
+    int erro = scanf("%d", &opcao);
+
+    if (erro != 1)
+    {
+        fflush(stdin);
+        opcao = -1;
+    }
+
+    return opcao;
+}
+
+int tela_menu_opcoes_admin()
 {
     int opcao;
 
@@ -110,6 +239,175 @@ int tela_menu_opcoes()
     return opcao;
 }
 
+void tela_cadastrar_admin()
+{
+    char ch;
+    int erro;
+    int idxPesquisa = -1;
+
+    char id[12];
+    char nome[256];
+    char senha[10];
+
+    limpar();
+    carregar_admin_arquivo();
+    printf("-- Menu Cadastrar Admin -- \n");
+
+    if (total_admin >= NUM_MAX_CADASTROS_ADMIN)
+    {
+        printf("\nErro: O cadastro já atingiu sua capacidade máxima!\n");
+        pausar();
+    }
+
+    do
+    {
+        erro = 0;
+
+        printf("\n*Digite a id da escola: ");
+        scanf(" %11[^\n]", id);
+        fflush(stdin);
+
+        if (strlen(id) > 11)
+        {
+            erro = 1;
+            printf("ERRO: O id pode possuir no máximo 11 carracteres!\n");
+        }
+        if (strlen(id) < 1)
+        {
+            erro = 1;
+            printf("ERRO: O id deve possuir pelo menos 1 carracter!\n");
+        }
+    } while (erro == 1);
+
+    do
+    {
+        erro = 0;
+
+        printf("*Digite o nome da escola: ");
+        scanf(" %255[^\n]", nome);
+        fflush(stdin);
+
+        if (strlen(nome) > 255)
+        {
+            erro = 1;
+            printf("ERRO: O nome da escola pode possuir no máximo 254 carracteres!\n");
+        }
+        if (strlen(nome) < 3)
+        {
+            erro = 1;
+            printf("ERRO: O nome da escola deve possuir pelo menos 3 carracteres!\n");
+        }
+    } while (erro == 1);
+
+    do
+    {
+        erro = 0;
+
+        printf("*Digite a senha: ");
+        scanf(" %9[^\n]", senha);
+        fflush(stdin);
+
+        if (strlen(senha) > 9)
+        {
+            erro = 1;
+            printf("ERRO: A senha pode possuir no máximo 9 carracteres!\n");
+        }
+        if (strlen(senha) < 4)
+        {
+            erro = 1;
+            printf("ERRO: A senha deve possuir pelo menos 4 carracteres!\n");
+        }
+    } while (erro == 1);
+
+    idxPesquisa = -1;
+    for (int i = 0; i < NUM_MAX_CADASTROS_ADMIN; i++)
+    {
+        if (vetor_admin[i].ocupado == 1)
+        {
+            if (strcmp(vetor_admin[i].id, id) == 0)
+            {
+                idxPesquisa = i;
+                break;
+            }
+        }
+    }
+
+    if (idxPesquisa != -1)
+    {
+        printf("\nErro: Não foi possível inserir os dados pois já existe uma escola com o mesmo id cadastrado!\v");
+        pausar();
+    }
+    else
+    {
+        for (int i = 0; i < NUM_MAX_CADASTROS_ADMIN; i++)
+        {
+            if (vetor_admin[i].ocupado == 0)
+            {
+                vetor_admin[i].ocupado = 1;
+
+                strcpy(vetor_admin[i].id, id);
+                strcpy(vetor_admin[i].nome, nome);
+                strcpy(vetor_admin[i].senha, senha);
+                total_admin = total_admin + 1;
+                salvar_admin_arquivo();
+
+                printf("\nDados cadastrados com sucesso! \n");
+                pausar();
+
+                break;
+            }
+        }
+    }
+}
+
+int tela_login_admin()
+{
+    char ch;
+    int erro;
+    int idxPesquisa = -1;
+    char id[12];
+    char senha[10];
+
+    limpar();
+    carregar_admin_arquivo();
+    printf("-- Menu Login Admin -- \n");
+    erro = 0;
+
+    printf("\nDigite o id: ");
+    scanf(" %11[^\n]", id);
+    fflush(stdin);
+    printf("Digite a senha: ");
+    scanf(" %9[^\n]", senha);
+    fflush(stdin);
+
+    for (int i = 0; i < total_admin; i++)
+    {
+        if (vetor_admin[i].ocupado == 1)
+        {
+            if (strcmp(vetor_admin[i].id, id) == 0 && strcmp(vetor_admin[i].senha, senha) == 0)
+            {
+                idxPesquisa = i;
+                break;
+            }
+        }
+    }
+
+    if (idxPesquisa != -1)
+    {
+        printf("\nLogin realizado com sucesso! Bem-vindo(a), %s.\n", vetor_admin[idxPesquisa].nome);
+        pausar();
+
+        return tela_menu_opcoes_admin();
+    }
+    else
+    {
+        printf("Id: %s", vetor_admin[0].id);
+        printf("Senha: %s", vetor_admin[0].senha);
+        printf("\nNão foi encontrado o admin com os dados informados!\n\n");
+        pausar();
+    }
+}
+
 void tela_cadastrar_aluno()
 {
     int continuar;
@@ -129,7 +427,7 @@ void tela_cadastrar_aluno()
         limpar();
         printf("-- Menu Cadastrar -- \n");
 
-        if (total_alunos >= NUM_MAX_CADASTROS)
+        if (total_alunos >= NUM_MAX_CADASTROS_ALUNOS)
         {
             printf("\nErro: O cadastro já atingiu sua capacidade m�xima!\n");
             pausar();
@@ -256,7 +554,7 @@ void tela_cadastrar_aluno()
         } while (erro == 1);
 
         idxPesquisa = -1;
-        for (int i = 0; i < NUM_MAX_CADASTROS; i++)
+        for (int i = 0; i < NUM_MAX_CADASTROS_ALUNOS; i++)
         {
             if (vetor_alunos[i].ocupado == 1)
             {
@@ -278,7 +576,7 @@ void tela_cadastrar_aluno()
         }
         else
         {
-            for (int i = 0; i < NUM_MAX_CADASTROS; i++)
+            for (int i = 0; i < NUM_MAX_CADASTROS_ALUNOS; i++)
             {
                 if (vetor_alunos[i].ocupado == 0)
                 {
@@ -413,7 +711,7 @@ void tela_remover()
         fflush(stdin);
 
         idxPesquisa = -1;
-        for (int i = 0; i < NUM_MAX_CADASTROS; i++)
+        for (int i = 0; i < NUM_MAX_CADASTROS_ALUNOS; i++)
         {
             if (vetor_alunos[i].ocupado == 1)
             {
@@ -469,18 +767,18 @@ void tela_editar()
 void tela_relatorio()
 {
     limpar();
-    printf("4 - Relatório \n\n");
+    printf("-- Menu Relatório Alunos -- \n\n");
 
     printf("Total de Alunos Cadastrados: %d\n\n", total_alunos);
 
-    for (int i = 0; i < NUM_MAX_CADASTROS; i++)
+    for (int i = 0; i < NUM_MAX_CADASTROS_ALUNOS; i++)
     {
         if (vetor_alunos[i].ocupado == 1)
         {
-            printf("Matrícula:     %s\n", vetor_alunos[i].matricula);
-            printf("Nome:    %s\n", vetor_alunos[i].nome);
-            printf("Idade:   %d\n", vetor_alunos[i].idade);
-            printf("Sexo:    %c\n", vetor_alunos[i].sexo);
+            printf("Matrícula: %s\n", vetor_alunos[i].matricula);
+            printf("Nome: %s\n", vetor_alunos[i].nome);
+            printf("Idade: %d\n", vetor_alunos[i].idade);
+            printf("Sexo: %c\n", vetor_alunos[i].sexo);
         }
     }
 
@@ -546,7 +844,7 @@ void salvar_alunos_arquivo()
     }
     fprintf(fp, "%d\n", total_alunos);
 
-    for (int i = 0; i < NUM_MAX_CADASTROS; i++)
+    for (int i = 0; i < NUM_MAX_CADASTROS_ALUNOS; i++)
     {
         if (vetor_alunos[i].ocupado == 1)
         {
@@ -582,6 +880,68 @@ void carregar_alunos_arquivo()
         fscanf(fp, "%999[^\n] ", aluno.informacoes);
         aluno.ocupado = 1;
         vetor_alunos[i] = aluno;
+    }
+    fclose(fp);
+}
+
+void init_arquivo_admin()
+{
+    strcpy(caminho_arquivo_admin, "admin.txt");
+    FILE *fp = fopen(caminho_arquivo_admin, "r");
+
+    if (fp == NULL)
+    {
+        fp = fopen(caminho_arquivo_admin, "w");
+        fprintf(fp, "%d\n", total_admin);
+        fclose(fp);
+    }
+    else
+    {
+        fclose(fp);
+    }
+}
+
+void salvar_admin_arquivo()
+{
+    FILE *fp = fopen(caminho_arquivo_admin, "w");
+
+    if (fp == NULL)
+    {
+        printf("Erro ao abrir o arquivo!");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(fp, "%d\n", total_admin);
+
+    for (int i = 0; i < NUM_MAX_CADASTROS_ADMIN; i++)
+    {
+        if (vetor_admin[i].ocupado == 1)
+        {
+            fprintf(fp, "%s\n", vetor_admin[i].id);
+            fprintf(fp, "%s\n", vetor_admin[i].nome);
+            fprintf(fp, "%s\n", vetor_admin[i].senha);
+        }
+    }
+    fclose(fp);
+}
+
+void carregar_admin_arquivo()
+{
+    FILE *fp = fopen(caminho_arquivo_admin, "r");
+    if (fp == NULL)
+    {
+        printf("Erro ao abrir o arquivo!");
+        exit(EXIT_FAILURE);
+    }
+    fscanf(fp, "%d ", &total_admin);
+
+    for (int i = 0; i < total_admin; i++)
+    {
+        Admin admin;
+        fscanf(fp, "%11[^\n] ", admin.id);
+        fscanf(fp, "%255[^\n] ", admin.nome);
+        fscanf(fp, "%9[^\n] ", admin.senha);
+        admin.ocupado = 1;
+        vetor_admin[i] = admin;
     }
     fclose(fp);
 }
